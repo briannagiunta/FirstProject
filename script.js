94b5d3ea86d0246262e66c56ba604de5c8c40590
Brianna Giunta <brig_oowee@Briannas-MacBook-Pro.local>
2021-03-23 14:04:41 -0400

    fixed restart button and changed background

--- script.js
+++ script.js
@@ -7,13 +7,14 @@ const gameScreen = document.querySelector(".game-screen");
 const currentPic = document.querySelector("#pic");
 const buttons = document.querySelector(".choices");
 const choices = document.querySelectorAll("button");
-const score = document.querySelector(".score");
+const score = document.querySelector("#score");
 const correctImg = document.querySelector(".correct")
 const incorrectImg = document.querySelector('.incorrect')
 const gameOverScreen = document.querySelector(".gameover")
 const over = document.querySelector(".again")
-const winScreen = document.querySelector(".winner")
+const winButton = document.querySelector(".winner")
 const playAgain = document.querySelector(".playagain")
+const timer = document.querySelector("#time")
 
 //game constants
 const nameArray = [];
@@ -25,8 +26,8 @@ const decoyNames = ["Past Rick", "Cop Rick", "Replacement Rick", "Insurance Rick
 class Ricks {
     constructor(name, pic){
         this.name = name,
-        this.pic = pic
-        nameArray.push(this.name)
+        this.pic = pic,
+        nameArray.push(this.name),
         picArray.push(this.pic)
     }
 } 
@@ -45,7 +46,6 @@ const rickDSanchez = new Ricks("Rick D Sanchez |||", "./assets/rickDsanchez3-rem
 
 //game variables
 let num = 25;
-let gameIsLive = false;
 let currentScore = 0;
 let randPic 
 let randChoice 
@@ -53,7 +53,9 @@ let userChoice = null;
 let currentChoice = false;
 let tempNameArray
 let tempPicArray
-let interval 
+let blurInterval 
+let timeInterval
+let time = 5
 
 
 
@@ -64,13 +66,12 @@ let interval
 // starts game
 const startGame = () =>{
     currentScore = 0;
-    gameIsLive = true;
     displayScreen.classList.add("hide")
     gameScreen.classList.remove("hide")
     gameOverScreen.classList.add("hide")
+    timer.classList.remove("hide")
+    resetArrays();
     levelUp()
-
-    
 }
 
 
@@ -79,16 +80,16 @@ const startGame = () =>{
 //adds pic to the screen && adds the name to a button then removes that name && pic from their arrays
 // creates a copy of decoy names..goes through each button and adds a decoy name then removes that name from the copied array so its not repeated.
 const levelUp = ()=>{
-    checkForWin();
     resetChoices();
+    unBlurPicture()
     currentChoice = false;
     let tempDecoyNames = decoyNames.map((x) => x)
-    currentRound = Math.floor(Math.random() * picArray.length)
+    currentRound = Math.floor(Math.random() * tempPicArray.length)
     randChoice = Math.floor(Math.random() * 4)
-    currentPic.src = picArray[currentRound]
-    choices[randChoice].innerHTML = nameArray[currentRound]
-    nameArray.splice(currentRound, 1)
-    picArray.splice(currentRound, 1)
+    currentPic.src = tempPicArray[currentRound]
+    choices[randChoice].innerHTML = tempNameArray[currentRound]
+    tempNameArray.splice(currentRound, 1)
+    tempPicArray.splice(currentRound, 1)
     choices.forEach(function(choice){
         let randNum = Math.floor(Math.random() * tempDecoyNames.length)
         if(choice.innerHTML){
@@ -98,50 +99,67 @@ const levelUp = ()=>{
             tempDecoyNames.splice(randNum, 1)
         }
     })
-    unBlurPicture()
+    
     
 }
 
-//removes blur from picture 
-//if blur is fully removed before correct guess is made, displays gameover screen
-//if correct guess is made, does nothing.
+// removes blur from picture 
+// if blur is fully removed before correct guess is made, displays gameover screen
+// if correct guess is made, does nothing.
+// changes timer to count down 5 seconds
 const unBlurPicture = () =>{ 
-    
-        interval = setInterval(() => {
+        blurInterval = setInterval(() => {
         blurryBlur.classList.remove(blurryBlur.classList[num])
         num = num - 1 
         if (blurryBlur.classList.length === 0 && currentChoice === false){
             gameScreen.classList.add("hide")
             gameOverScreen.classList.remove("hide")
-            clearInterval(interval)
-            gameIsLive = false;
+            clearInterval(blurInterval)
+            
         } else{
             return
         }
     }, 200);
-    
-}
-//resets the blur && clears interval
-const blurPicture = ()=>{
-    for(let i=1; i<25;i++){
-        blurryBlur.classList.add(`b${i}`)
+        timeInterval = setInterval(() =>{
+            time--
+            timer.innerHTML = `Timer: ${time}`
+        },1000)
     }
+
+    //resets the blur && clears interval
+    const blurPicture = ()=>{
+        for(let i=1; i<25;i++){
+            blurryBlur.classList.add(`b${i}`)
+        }
         num = 25;
-        clearInterval(interval)
-        gameIsLive = false;
+        clearInterval(blurInterval)
+        clearInterval(timeInterval)
+        time= 5
+        timer.innerHTML = "Timer: 5"
 }
   //resets choices to nothing so they will chance each time
 const resetChoices = () =>{
     choices.forEach(function(choice){
-        choice.innerHTML = null;
-        
+        choice.innerHTML = null;  
     })
+}
+//resets arrays for start over buttons
+const resetArrays = () =>{
+    tempNameArray = nameArray.map((x) => x)
+    tempPicArray = picArray.map((x) => x)
 }
 
 const checkForWin = () =>{
     if(currentScore === 10){
-        score.innerHTML = "Wubba Lubba Dub Dub !!!";
+        score.innerHTML = "We did it Morty! -burps- Alright, Im gonna go take a s#!t.";
         blurPicture()
+        winButton.classList.remove("hide")
+        timer.classList.add("hide")
+        choices.forEach(function(choice){
+            choice.innerHTML = "Wubba Lubba Dub Dub!"
+        })
+    } else{
+        return
     }
 }
 
@@ -149,27 +167,25 @@ const checkForWin = () =>{
 
 //event listeners
 startButton.addEventListener('click', () =>{
+    currentScore = 0
     currentChoice = false;
-    gameIsLive = true;
     // blurPicture()
     startGame()
 })
 //if correct answer is clicked, correct gif shows, score increases by 1, goes to next level and blurs picture 
 buttons.addEventListener('click',(e) =>{
     userChoice = e.target;
-        if(userChoice.innerHTML === choices[randChoice].innerHTML){
-            gameIsLive = true;
-            currentScore++
-            score.innerHTML = `score: ${currentScore}`;
-            correctImg.src = "https://media3.giphy.com/media/Qs0QEnugOy0xIsFkpD/giphy.gif?cid=ecf05e47bc384g5m0ghjqua80ag5pfs8zh32r0r2hd5ccs9a&rid=giphy.gif"
-            levelUp()
-            blurPicture()
-            
-            
-        }else{
-            currentChoice = false;
-            correctImg.src = "https://media1.giphy.com/media/JRsLN0zkMeR8ryVN2J/giphy.gif?cid=ecf05e477f27i20b14zsvybv0xq55sksxizz3yatrrcnjdr2&rid=giphy.gif"
-        }
+    if(userChoice.innerHTML === choices[randChoice].innerHTML){
+        currentScore++
+        score.innerHTML = `score: ${currentScore}`;
+        correctImg.src = "https://media3.giphy.com/media/Qs0QEnugOy0xIsFkpD/giphy.gif?cid=ecf05e47bc384g5m0ghjqua80ag5pfs8zh32r0r2hd5ccs9a&rid=giphy.gif"
+        blurPicture()
+        levelUp()
+    }else{
+        currentChoice = false;
+        correctImg.src = "https://media1.giphy.com/media/JRsLN0zkMeR8ryVN2J/giphy.gif?cid=ecf05e477f27i20b14zsvybv0xq55sksxizz3yatrrcnjdr2&rid=giphy.gif"
+    }
+    checkForWin();
 })
 
 over.addEventListener('click', () =>{
@@ -181,14 +197,19 @@ over.addEventListener('click', () =>{
     currentScore = 0;
     score.innerHTML = `score: ${currentScore}`;
     currentChoice = true;
-    gameIsLive = false;
+    startGame();
+    winButton.classList.add("hide")
 })
 
-// playAgain.addEventListener('click', () =>{
-//     blurPicture();
-//     winScreen.classList.add("hide")
-//     displayScreen.classList.add('hide')
-//     gameScreen.classList.remove('.hide')
-//     currentScore = 0
-//     gameIsLive = false;
-// })
+winButton.addEventListener('click', () =>{
+    blurPicture()
+    gameOverScreen.classList.add("hide")
+    displayScreen.classList.add("hide")
+    gameScreen.classList.remove('hide')
+    correctImg.src = ""
+    currentScore = 0;
+    score.innerHTML = `score: ${currentScore}`;
+    currentChoice = true;
+    startGame();
+    winButton.classList.add("hide")
+})
